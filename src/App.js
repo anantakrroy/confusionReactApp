92fcd6e85b9d16b520231e67d04b9bfc87abfcc7
Ananta Kr. Roy <kr.innovate@gmail.com>
2020-08-30 02:21:19 +0530

    Lifted state up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
-import { render } from 'react-dom';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
-      </div>
-    );
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        dishes : DISHES
+      }
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes}/>
+        </div>
+      );
+    }
   }
-}
 
 export default App;
