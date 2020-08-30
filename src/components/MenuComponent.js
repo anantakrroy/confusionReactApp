31bcfbfd6eee8f1f0a2d7eeea67528b1330b93e3
Ananta Kr. Roy <kr.innovate@gmail.com>
2020-08-30 19:53:51 +0530

    Added dish detail component for comments and dish details view

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DishDetail from './DishDetailComponent';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
@@ -15,24 +16,6 @@ class Menu extends Component {
         this.setState({
             selectedDish: dish
         })
-    }
-
-    renderDish(dish) {
-        if (dish !== null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        } else {
-            return (
-                <div></div>
-            )
-        }
     }
 
     render() {
