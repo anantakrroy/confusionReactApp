9c756c6b0bd1f0a09e4d926b9d949d22a9cf5b47
Ananta Kr. Roy <kr.innovate@gmail.com>
2020-09-05 02:02:08 +0530

    Added dummy user comments

---
+++ src/shared/comments.js
@@ -0,0 +1,32 @@
+export const COMMENTS = [
+    {
+        id : 1,
+        comment : 'Imagine all the eatables living in confusion',
+        user : 'John Lennon',
+        date : 'Oct 17, 2012'
+    },
+    {
+        id : 2,
+        comment : 'Imagine all the eatables living in confusion',
+        user : 'John Lennon',
+        date : 'Oct 17, 2012'
+    },
+    {
+        id : 3,
+        comment : 'Imagine all the eatables living in confusion',
+        user : 'John Lennon',
+        date : 'Oct 17, 2012'
+    },
+    {
+        id : 4,
+        comment : 'Imagine all the eatables living in confusion',
+        user : 'John Lennon',
+        date : 'Oct 17, 2012'
+    },
+    {
+        id : 5,
+        comment : 'Imagine all the eatables living in confusion',
+        user : 'John Lennon',
+        date : 'Oct 17, 2012'
+    }
+]
